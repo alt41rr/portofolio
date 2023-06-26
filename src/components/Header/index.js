108d586a1a3cf75af4b33271e2d45266d4da0800
alt41rr <kandoudavid1@gmail.com>
2023-06-26 18:15:21 +0800

    Menambahkan Folder Assets dan isinya, Menambahkan component

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <header id="header">
+            <div className="container">
+                <h1><a href="index.html">Emily Jones</a></h1>
+                {/* <a href="index.html" className="mr-auto"><img src="assets/img/logo.png" alt className="img-fluid" /></a> */}
+                <h2>I'm a passionate <span>graphic designer</span> from New York</h2>
+                <div className="social-links">
+                <a href="/#" className="twitter"><i className="bi bi-twitter" /></a>
+                <a href="/#" className="facebook"><i className="bi bi-facebook" /></a>
+                <a href="/#" className="instagram"><i className="bi bi-instagram" /></a>
+                <a href="/#" className="linkedin"><i className="bi bi-linkedin" /></a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default index
